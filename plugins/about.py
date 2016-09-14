43ee2d6b2b8393100640f6d0447ccd4b896705ba
ali4689y <www.zonerteam@gmail.com>
2016-09-15 02:13:35 +0430

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -5,7 +5,7 @@ from message import Message
 @asyncio.coroutine
 def run(message, matches, chat_id, step):
     return [Message(chat_id).set_text(
-        "Siarobo Developed By *Siyanew*\n_For further information join_ @siyanews _Channel!_", parse_mode="markdown")]
+        "*Bot Search Music!*\n_✔Fast and accurate✔_\n__", parse_mode="markdown")]
 
 
 plugin = {
